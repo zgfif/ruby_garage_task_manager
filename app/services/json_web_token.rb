7830c2e7dfe210c055851fb21e33b38f1410154b
Pavel <zihzag@gmail.com>
2020-09-29 14:56:35 +0300

    #34: change credentilas

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -4,8 +4,7 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      # JWT.encode(payload, ENV['SECRET_KEY_BASE'])
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
 
     def decode(token)
